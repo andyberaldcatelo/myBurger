9aa88f6af53ec83854b922d14244a200d97eb305
andyberaldcatelo <andy.berald@yahoo.fr>
2020-09-24 08:01:34 +0200

    2020 09 24 Handling Errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class BurgerBuilder extends Component {
   INGREDIENTS_PRICE = {
@@ -97,7 +98,7 @@ class BurgerBuilder extends Component {
       deliveryMethod: 'fastest',
     };
     axios
-      .post('/orders.json', order)
+      .post('/ordersn', order)
       .then((response) => this.setState({ loading: false, purchasing: false }))
       .catch((error) => this.setState({ loading: false, purchasing: false }));
   };
@@ -144,4 +145,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Auxiliary/Auxiliary';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null,
+    };
+
+    confirmedErrorHandler = () => {
+      this.setState({ error: null });
+    };
+
+    componentDidMount() {
+      axios.interceptors.request.use((req) => {
+        this.setState({ error: null });
+        return req;
+      });
+      axios.interceptors.response.use(
+        (response) => response,
+        (error) => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    render() {
+      return (
+        <Aux>
+          <Modal
+            modalClosed={this.confirmedErrorHandler}
+            show={this.state.error}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
